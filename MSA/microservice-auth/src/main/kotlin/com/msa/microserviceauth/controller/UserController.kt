0394ce39ac35b72b552b186e42167d37db770c03
kim1387 <kevink0@naver.com>
2021-12-06 14:26:58 +0900

    add health_check function and add Timed log for prometheus

--- MSA/microservice-auth/src/main/kotlin/com/msa/microserviceauth/controller/UserController.kt
+++ MSA/microservice-auth/src/main/kotlin/com/msa/microserviceauth/controller/UserController.kt
@@ -7,9 +7,9 @@ import com.msa.microserviceauth.domain.User
 import com.msa.microserviceauth.dto.MyPageDto
 import com.msa.microserviceauth.dto.SignInDto
 import com.msa.microserviceauth.dto.SignUpDto
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import io.micrometer.core.annotation.Timed
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -19,21 +19,43 @@ import javax.validation.Valid
 class UserController(
         @Autowired private val signUpService: SignUpService,
         @Autowired private val signInService: SignInService,
-        @Autowired private val myInfoService: MyInfoService
+        @Autowired private val myInfoService: MyInfoService,
+        @Autowired private val env: Environment
 ) {
+    /**
+     * user status check function
+     * @author 기현
+     */
+    @GetMapping("/health_check")
+    @Timed(value = "user.status", longTask = true)
+    fun status(): String {
+        return String.format(
+            "It's Working in User Service"
+                    + ", port(local.server.port)=" + env.getProperty("local.server.port")
+                    + ", port(server.port)=" + env.getProperty("server.port")
+                    + ", gateway ip=" + env.getProperty("gateway.ip")
+                    + ", message=" + env.getProperty("greeting.message")
+                    + ", token secret=" + env.getProperty("token.secret")
+                    + ", token expiration time=" + env.getProperty("token.expiration_time")
+        )
+    }
+
     @PostMapping("/register")
+    @Timed(value = "user.register", longTask = true) // 기현
     fun register(@RequestBody @Valid signUpReq: SignUpDto.SignUpReq): ResponseEntity<User> {
         val user = signUpService.signUp(signUpReq)
         return ResponseEntity(user, HttpStatus.CREATED)
     }
 
     @PostMapping("/login")
+    @Timed(value = "user.login", longTask = true)// 기현
     fun login(@RequestBody @Valid signInReq: SignInDto.SignInReq): ResponseEntity<SignInDto.SignInResult> {
         val signInResult = signInService.signIn(signInReq)
         return ResponseEntity(signInResult, HttpStatus.OK)
     }
 
     @GetMapping("/my_page")
+    @Timed(value = "user.my_page", longTask = true)// 기현
     fun myPage(@RequestHeader("username") username: String): ResponseEntity<MyPageDto.MyPageResult> {
         val userInfo = myInfoService.myInfo(username)
 
